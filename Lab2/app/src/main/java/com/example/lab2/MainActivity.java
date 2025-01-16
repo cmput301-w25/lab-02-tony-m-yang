ff8aaeab6aeafacff9ca86032c5650f70f3cfe50
tony-m-yang <tmyang@ualberta.ca>
2025-01-15 19:15:25 -0700

    Added add city button and delete button
    not properly implemented yet, add just adds a city called new city and delete gets rid of the last city in the list

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,20 +1,20 @@
 package com.example.lab2;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     // Variables
+    Button addButton, deleteButton;
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
@@ -22,18 +22,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        //access ListView using it's ID
         cityList = findViewById(R.id.city_list);
-        
 
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        //define some data
+        String [] cities = {"Edmonton","Calgary","Vancouver","Montreal","Sydney"};
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        //initialize adapter with data from cities
+        //gets the layout from content.xml
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        //access the buttons using their IDs
+        addButton = findViewById(R.id.addButtonId);
+        deleteButton = findViewById(R.id.deleteButtonId);
+
+        //on button click add a city
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dataList.add("New city"); // Change later
+                cityAdapter.notifyDataSetChanged();
+            }
         });
-         */
+
+        //on button click delete a city
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dataList.remove(dataList.size() - 1); // Change later
+                cityAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
 }
